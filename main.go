6886cb9092ec80e12e683250d49cee31fc54745b
jack410 <jack410@163.com>
2023-10-20 18:21:57 +0800

    write user response to mysql

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"log"
 	"math/rand"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 )
@@ -16,6 +20,7 @@ type Answers struct {
 	OriginChannelId string
 	FavFood         string
 	FavGame         string
+	RecordId        int64
 }
 
 func (a *Answers) ToMessageEmbed() discordgo.MessageEmbed {
@@ -27,6 +32,10 @@ func (a *Answers) ToMessageEmbed() discordgo.MessageEmbed {
 		{
 			Name:  "Favorite game",
 			Value: a.FavFood,
+		},
+		{
+			Name:  "Record id",
+			Value: strconv.FormatInt(a.RecordId, 10),
 		},
 	}
 
@@ -51,6 +60,12 @@ func main() {
 		log.Fatal(err)
 	}
 
+	db, err := sql.Open("mysql", os.Getenv("DSN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
 		//发送信息的id和session的id一致则不处理信息直接退出
 		if m.Author.ID == s.State.User.ID {
@@ -60,25 +75,7 @@ func main() {
 		//DM logic
 		//如果m.GuildID为空则认为这个消息是direct message
 		if m.GuildID == "" {
-			answers, ok := responses[m.ChannelID]
-			if !ok {
-				return
-			}
-			if answers.FavFood == "" {
-				answers.FavFood = m.Content
-
-				s.ChannelMessageSend(m.ChannelID, "Great! What's your favorite game now?")
-
-				responses[m.ChannelID] = answers
-				return
-			} else {
-				answers.FavGame = m.Content
-				//log.Printf("answers: %v, %v", answers.FavFood, answers.FavGame)
-				embed := answers.ToMessageEmbed()
-				sess.ChannelMessageSendEmbed(answers.OriginChannelId, &embed)
-
-				delete(responses, m.ChannelID)
-			}
+			UserPromptResponseHandler(db, s, m)
 		}
 
 		//server logic
@@ -140,6 +137,46 @@ func main() {
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
+}
+
+func UserPromptResponseHandler(db *sql.DB, s *discordgo.Session, m *discordgo.MessageCreate) {
+	answers, ok := responses[m.ChannelID]
+	if !ok {
+		return
+	}
+	if answers.FavFood == "" {
+		answers.FavFood = m.Content
+
+		s.ChannelMessageSend(m.ChannelID, "Great! What's your favorite game now?")
+
+		responses[m.ChannelID] = answers
+		return
+	} else {
+		answers.FavGame = m.Content
+
+		query := "insert into discord_message (payload, user_id) values(?,?)"
+		jbytes, err := json.Marshal(answers)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		res, err := db.Exec(query, string(jbytes), m.ChannelID)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		lastInsertId, err := res.LastInsertId()
+		if err != nil {
+			log.Fatal(err)
+		}
+		answers.RecordId = lastInsertId
+
+		//log.Printf("answers: %v, %v", answers.FavFood, answers.FavGame)
+		embed := answers.ToMessageEmbed()
+		s.ChannelMessageSendEmbed(answers.OriginChannelId, &embed)
+
+		delete(responses, m.ChannelID)
+	}
 }
 
 func UserPromptHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
